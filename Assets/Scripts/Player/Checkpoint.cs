4f4851cbf22c2170beeda2969f32df096f729180
Amon De Shir <amonsir29@gmail.com>
2024-06-11 23:54:42 +0200

    add sound to checkpoint

--- Assets/Scripts/Player/Checkpoint.cs
+++ Assets/Scripts/Player/Checkpoint.cs
@@ -6,17 +6,23 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     public Color Active;
+    public AudioSource sound;
 
     private SpriteRenderer sprite;
 
+    private bool activated;
+
     public void Start() {
         sprite = GetComponent<SpriteRenderer>();
+        activated = false;
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.TryGetComponent(out PlayerKillable entity)) {
+        if (!activated && collider.TryGetComponent(out PlayerKillable entity)) {
             entity.SetCheckPoint(this.transform.position);
             sprite.color = Active;
+            sound.Play();
+            activated = true;
         }
     }
 }
