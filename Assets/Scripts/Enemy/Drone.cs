7ae5dc57a4e1047916f6a6126da6634962e74737
Amon De Shir <amonsir29@gmail.com>
2024-06-12 10:20:38 +0200

    remove unity

--- Assets/Scripts/Enemy/Drone.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using UnityEngine;
-
-[RequireComponent(typeof(DroneEnemyController))]
-public class Drone : Enemy { }
--- Assets/Scripts/Enemy/EnemyController.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class EnemyController: MonoBehaviour
-{
-    public abstract void PlanPath(Vector3 targetPosition);
-    public abstract void Stop();
-    public abstract bool IsOnTarget();
-}
